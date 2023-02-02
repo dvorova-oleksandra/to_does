53b9bab259228fa8b1e10ef4bb06e2e0ec7561c5
Oleksandra Dvorova <sashadvorovay@gmail.com>
2023-02-02 13:25:23 +0200

    Improve code. Create new model for functions for working with file. Using try and except and fixing bags.

--- Command_line/main.py
+++ Command_line/main.py
@@ -1,34 +1,49 @@
-with open('files/to_do.txt', 'r') as files:
-    to_does = files.readlines()
+from work_with_files import read_to_does_from_file, change_to_does_in_file
 
 while True:
     action = input("What do you want to do(add, complete, show, edit or exit)?Write one of this action ")
     match action.strip().lower():
         case "add":
             adding = input("Enter a todo: ") + "\n"
+            to_does = read_to_does_from_file()
             to_does.append(adding)
-            with open('files/to_do.txt', 'w') as files:
-                files.writelines(to_does)
+            change_to_does_in_file(to_does)
         case "complete":
-            delete_number = int(input("Enter number, which element you want to delete ")) - 1
-            delete_element = to_does[delete_number].strip('\n')
-            print(f'Todo {delete_element} was removed ')
-            to_does.pop(delete_number)
-            with open('files/to_do.txt', 'w') as files:
-                files.writelines(to_does)
+            try:
+                delete_number = int(input("Enter number, which element you want to delete ")) - 1
+                to_does = read_to_does_from_file()
+                delete_element = to_does[delete_number].strip('\n')
+                print(f'Todo {delete_element} was removed ')
+                to_does.pop(delete_number)
+                change_to_does_in_file(to_does)
+            except ValueError:
+                print("Invalid data. You must write number of todo")
+                continue
+            except IndexError:
+                print("This element isn't in list")
+                continue
         case 'show':
+            to_does = read_to_does_from_file()
             for index, item in enumerate(to_does):
                 item = item.strip('\n')
-                print(f'{index+1} - {item}')
+                print(f'{index + 1} - {item}')
         case "edit":
-            edit_number = int(input("Enter number, which element you want to edit "))
-            edit_element = to_does[edit_number-1].strip('\n')
-            print(f'This todo will be change: {edit_element}')
-            to_does[edit_number-1] = input("Write changed todo: ") + "\n"
-            with open('files/to_do.txt', 'w') as files:
-                files.writelines(to_does)
+            try:
+                to_does = read_to_does_from_file()
+                edit_number = int(input("Enter number, which element you want to edit "))
+                edit_element = to_does[edit_number - 1].strip('\n')
+                print(f'This todo will be change: {edit_element}')
+                to_does[edit_number - 1] = input("Write changed todo: ") + "\n"
+                change_to_does_in_file(to_does)
+            except ValueError:
+                print("Invalid data. You must write number of todo")
+                continue
+            except IndexError:
+                print("This element isn't in list")
+                continue
         case 'exit':
-            print('Bye!')
             break
         case _:
             print('Wrong input! Please enter correct data')
+
+print('Bye!')
