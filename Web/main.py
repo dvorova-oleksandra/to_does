686a0b073b69d8eb2b5c07ff7874aecffbea445d
Oleksandra Dvorova <sashadvorovay@gmail.com>
2023-02-06 02:00:27 +0200

    Add functional for add todo

--- Web/main.py
+++ Web/main.py
@@ -1,11 +1,22 @@
 import streamlit as interface
 import work_with_files
 
+todoes = work_with_files.read_to_does_from_file()
+
+# add new to do in list
+def add_todo():
+    todo = interface.session_state['new_todo'] +"\n"
+    todoes.append(todo)
+    work_with_files.change_to_does_in_file(todoes)
+
+
+#interface of program
 interface.title("My simple Todo App")
 interface.subheader("To do:")
-todoes = work_with_files.read_to_does_from_file()
+
 for todo in todoes:
     interface.checkbox(todo)
 
-interface.text_input(label="", placeholder="Write new todo")
+interface.text_input(label="", placeholder="Write new todo",
+                     on_change=add_todo, key='new_todo')
 
